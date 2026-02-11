d7cf2ca74206e3c9f1a2cdb5365e242abbd06a1a
sahanareddy594-dotcom <sahanareddy594@gmail.com>
2026-02-11 18:02:46 +0530

    Create set_operations.py

---
+++ Day2_Dictionaries and sets/set_operations.py
@@ -0,0 +1,11 @@
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+
+print("Set 1:", set1)
+print("Set 2:", set2)
+
+print("\nUnion:", set1 | set2)
+print("Intersection:", set1 & set2)
+print("Difference (set1 - set2):", set1 - set2)
+print("Difference (set2 - set1):", set2 - set1)
+print("Symmetric Difference:", set1 ^ set2)
