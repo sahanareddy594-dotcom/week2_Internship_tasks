f65722409ba3d0ba450ef66c291b0669d7c7b8cd
sahanareddy594-dotcom <sahanareddy594@gmail.com>
2026-02-11 17:52:52 +0530

    Create dict_student.py

---
+++ Day2_Dictionaries and sets/dict_student.py
@@ -0,0 +1,10 @@
+student = {
+    "Name": "Sahana",
+    "Age": 20,
+    "Course": "BCA",
+    "Year": "3rd Year",
+    "Percentage": 85.5
+}
+print("Student Details:")
+for key, value in student.items():
+    print(key, ":", value)
