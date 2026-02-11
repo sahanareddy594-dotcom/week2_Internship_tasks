8ba391de54070b9431bc6fc6188c919bcfcfded9
sahanareddy594-dotcom <sahanareddy594@gmail.com>
2026-02-11 17:55:57 +0530

    Create add_update_delete.py

---
+++ Day2_Dictionaries and sets/add_update_delete.py
@@ -0,0 +1,13 @@
+student = {
+    "Name": "Sahana",
+    "Age": 20,
+    "Course": "BCA"
+}
+
+print("Original Dictionary:", student)
+student["Percentage"] = 88
+print("\nAfter Adding Percentage:", student)
+student["Age"] = 21
+print("\nAfter Updating Age:", student)
+del student["Course"]
+print("\nAfter Deleting Course:", student)
