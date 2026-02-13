a9f903ea2f1f2d0a031bbf2c4ab2c0712bb73e5b
sahanareddy594-dotcom <sahanareddy594@gmail.com>
2026-02-13 16:14:41 +0530

    Create create_write.py

---
+++ Day4_File_Handling/create_write.py
@@ -0,0 +1,8 @@
+file = open("sample.txt", "w") 
+
+file.write("Hello, this is my first file.\n")
+file.write("I am learning Python file handling.")
+
+file.close()
+
+print("File created and data written successfully.")
