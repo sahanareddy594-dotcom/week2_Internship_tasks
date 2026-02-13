4ed76be00156f107da4b30649f9ae4361f235ed5
sahanareddy594-dotcom <sahanareddy594@gmail.com>
2026-02-13 16:19:56 +0530

    Create Append_data.py

---
+++ Day4_File_Handling/Append_data.py
@@ -0,0 +1,8 @@
+file = open("sample.txt", "a")
+
+file.write("\nThis data is appended to the file.")
+file.write("\nPython file handling is easy to learn.")
+
+file.close()
+
+print("Data appended successfully.")
