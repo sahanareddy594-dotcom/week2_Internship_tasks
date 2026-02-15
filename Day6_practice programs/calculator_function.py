97aead6259ea6cf1da3102098bc40855afe5c715
sahanareddy594-dotcom <sahanareddy594@gmail.com>
2026-02-15 16:37:09 +0530

    Create calculator_function.py

---
+++ Day6_practice programs/calculator_function.py
@@ -0,0 +1,45 @@
+# Functions for operations
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Error! Division by zero not allowed."
+    return a / b
+
+
+# Main program
+while True:
+    print("\n===== CALCULATOR =====")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "5":
+        print("Calculator closed.")
+        break
+
+    if choice in ["1", "2", "3", "4"]:
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == "1":
+            print("Result:", add(num1, num2))
+        elif choice == "2":
+            print("Result:", subtract(num1, num2))
+        elif choice == "3":
+            print("Result:", multiply(num1, num2))
+        elif choice == "4":
+            print("Result:", divide(num1, num2))
+    else:
+        print("Invalid choice! Try again.")
